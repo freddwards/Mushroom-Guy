b253a0d884d19a2b4183b8cfb0e497f13cdd9633
freddwards <freddieedwards2006@gmail.com>
2025-03-17 06:28:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+import os
+import csv
 
-from constants import SCREEN_HEIGHT
 from vector import Vector
 from player import Player
 from moss import Moss
@@ -21,29 +22,39 @@ class Game:
         self.player = Player(Vector(200, 150))
 
         # declaring several moss objects
-        self. moss = [
+        self.moss = [
             # position (x, y) , height , width
-             Moss(Vector(200, 600),10,200 ),
-             Moss(Vector(50, 600),10,100 ),
-             Moss(Vector(500, 600),10 , 50 )
+            Moss(Vector(200, 600), 10, 200),
+            Moss(Vector(50, 600), 10, 100),
+            Moss(Vector(500, 600), 10, 50)
         ]
 
+        # Initialize tile_list and world_data
+        self.tile_list = []
+        self.world_data = []
 
+        # Initialize screen_scroll
+        self.screen_scroll = 0
+
+        # Load images and level data
+        self.loadImages()
+        self.loadLevel()
 
     def draw(self, canvas):
         self.player.draw(canvas)
 
         # drawing the moss objects
         for moss in self.moss:
-             moss.draw(canvas)
+            moss.draw(canvas)
 
         self.update()
 
     def update(self):
         # call move method for player based on player inputs
-        self.player.move(self.left, self.right, self.jump)
+        self.screen_scroll = self.player.move(self.left, self.right, self.jump)  # takes output for moving screen
+        print(self.screen_scroll)
 
-         # checking if the player is in contact with the moss
+        # checking if the player is in contact with the moss
         on_moss = False
         for moss in self.moss:
             if moss.is_player_on_moss(self.player):
@@ -53,9 +64,6 @@ class Game:
         if on_moss:
             self.player.vel.x *= constants.SLOW_FACTOR  # Reduce horizontal velocity
             self.player.vel.y *= constants.SLOW_FACTOR  # Reduce vertical velocity (optional)
-
-
-
 
     def keyDown(self, key):  # taking inputs from the player
         if key == simplegui.KEY_MAP['d']:
@@ -72,6 +80,24 @@ class Game:
             self.left = False
         elif key == simplegui.KEY_MAP['w']:
             self.jump = False
+
+    def loadImages(self):
+        # Load tilemap images
+        for x in range(constants.TILE_TYPES):
+            absolute_path = os.path.abspath(f"assets/tiles/{x}.png")
+            image = simplegui.load_image(absolute_path)
+            self.tile_list.append(image)
+
+    def loadLevel(self):
+        for row in range(150):  # creating empty 150x150 world_data list
+            r = [-1] * 150
+            self.world_data.append(r)
+        # loading level data from csv file into world_data
+        with open("levels/level0_data.csv", newline="") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            for x, row in enumerate(reader):
+                for y, tile in enumerate(row):
+                    self.world_data[x][y] = int(tile)
 
 
 frame = simplegui.create_frame("Mushroom Guy", constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
