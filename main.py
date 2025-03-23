975fa17ee37017ef4393a05856165906013be161
freddwards <freddieedwards2006@gmail.com>
2025-03-23 02:49:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import math
 
 from vector import Vector
 from player import Player
+from level import Level
 from level import Block
-from moss import Moss
-
 import constants
+from player import Health
+
+
 
 
 class Game:
@@ -19,102 +21,53 @@ class Game:
         self.right = False
         self.left = False
         self.jump = False
-        self.damaged_cooldown = 0  # Prevents continuous damage
         self.blocks = []
         self.moss = []
-        
-        # declaring player object
-        self.player = Player(Vector(15, 150))
 
-        self.interaction = Interaction(self)
-
-        # declaring several moss objects
-        self.moss = [
-            # position (x, y) , height , width
-            Moss(Vector(200, 600), 10, 200),
-            Moss(Vector(50, 600), 10, 100),
-            Moss(Vector(500, 600), 10, 50)
-        ]
-
-        self.blocks = [
-            Block((500,500)),
-            Block((200,400)),
-            Block((300,300)),
-        ]
-
-        # Initialize tile_list and world_data
-        self.tile_list = []
-        self.world_data = []
-
-        # Initialize screen_scroll
-        self.screen_scroll = 0
-
-        # Load images and level data
-        self.loadImages()
+        # Define world data
+        self.world_data = world_data = []
         self.loadLevel()
+        self.screen_scroll = [0, 0]
+
+        self.tile_list = []
+        self.loadImages()
+
+        # Declare player
+        self.player = Player(Vector(200, 150))
+        self.interaction = Interaction(self)
+        # Declare level
+        self.level = Level()
+        # Process level data
+        self.level.process_data(self.world_data, self.tile_list)
 
     def draw(self, canvas):
-        if self.player.health.current_health > 0:
-            self.interaction.check_and_handle_collisions()
-
-            self.player.draw(canvas)
-
-            # drawing the moss objects
-            for moss in self.moss:
-                moss.draw(canvas)
-
-            for block in self.blocks:
-                block.draw(canvas)
-
-            self.update()
-        else:
-            canvas.draw_text("GAME OVER", (constants.SCREEN_WIDTH // 2 - 100, constants.SCREEN_HEIGHT // 2),
-                             40, "Red", "sans-serif")
+        self.level.update(self.screen_scroll)
+        self.level.draw(canvas)
+        self.player.draw(canvas)
+        self.update()
+        for block in self.blocks:
+            block.draw(canvas)
 
     def update(self):
-        # call move method for player based on player inputs
-        self.screen_scroll = self.player.move(self.left, self.right, self.jump)  # takes output for moving screen
+        # Call move method for player based on player inputs
+        self.screen_scroll = self.player.move(self.left, self.right, self.jump) # takes output for moving screen
+        self.interaction.check_and_handle_collisions()
 
-        # checking if the player is in contact with the moss
-        on_moss = False
-        for moss in self.moss:
-            if moss.is_player_on_moss(self.player):
-                on_moss = True
-
-                break
-        self.player.on_moss = on_moss
-
-        if on_moss:
-
-            self.player.vel.x *= constants.SLOW_FACTOR  # Reduce horizontal velocity
-            self.player.vel.y *= constants.SLOW_FACTOR  # Reduce vertical velocity (optional)
-
-            if self.damaged_cooldown == 0:  #Prevents immediate life loss
-                self.damaged_player()
-                self.damaged_cooldown = 180  #A delay before next damage
-
-        #Decrease cooldown timer
-        if self.damaged_cooldown > 0:
-            self.damaged_cooldown -= 1
-
-    def keyDown(self, key):  # taking inputs from the player
+    def keyDown(self, key):  # Handle key presses
         if key == simplegui.KEY_MAP['d']:
             self.right = True
         elif key == simplegui.KEY_MAP['a']:
             self.left = True
-        elif key == simplegui.KEY_MAP['w']:
+        elif key == simplegui.KEY_MAP['space']:
             self.jump = True
 
-    def keyUp(self, key):  # ending inputs from the player
+    def keyUp(self, key):  # Handle key releases
         if key == simplegui.KEY_MAP['d']:
             self.right = False
         elif key == simplegui.KEY_MAP['a']:
             self.left = False
-        elif key == simplegui.KEY_MAP['w']:
+        elif key == simplegui.KEY_MAP['space']:
             self.jump = False
-
-    def damaged_player(self):
-        self.player.health.life_lost()
 
     def loadImages(self):
         # Load tilemap images
@@ -124,88 +77,105 @@ class Game:
             self.tile_list.append(image)
 
     def loadLevel(self):
-        for row in range(150):  # creating empty 150x150 world_data list
+        for row in range(150): # creating empty 150x150 world_data list
             r = [-1] * 150
             self.world_data.append(r)
-        # loading level data from csv file into world_data
+        #loading level data from csv file into world_data
         with open("levels/level0_data.csv", newline="") as csvfile:
             reader = csv.reader(csvfile, delimiter=",")
             for x, row in enumerate(reader):
                 for y, tile in enumerate(row):
                     self.world_data[x][y] = int(tile)
 
+
 class Interaction:
     def __init__(self, game):
         self.game = game
 
+
     def check_and_handle_collisions(self):
+        self.handle_tile_collisions()  # tile collisions
+        # self.handle_moss_collisions()
 
-        self.handle_block_collisions()
-        #self.handle_moss_collisions()
 
-    def handle_block_collisions(self):
+    # def handle_moss_collisions(self):
+    #    for moss in self.game.moss:
+    #        if moss.is_player_on_moss(self.game.player):
+    #            self.game.player.on_moss = True
 
-        self.game.player.on_block = False  # assume the player is not on the block
-        for block in self.game.blocks:
-            if self.is_colliding_with_block(self.game.player, block):
-                self.resolve_block_collision(self.game.player, block)
-                if self.game.player.is_on_block(block):
-                    self.game.player.on_block = True  # player is on top of the block
+    def handle_tile_collisions(self):
+        self.game.player.on_ground = False  # assume the player is not on the tile
 
-    def handle_moss_collisions(self):
+        for tile in self.game.level.map_tiles:
+            if self.is_colliding_with_tile(self.game.player, tile):
+                self.resolve_tile_collision(self.game.player, tile)
 
-       for moss in self.game.moss:
-           if moss.is_player_on_moss(self.game.player):
-               self.game.player.on_moss = True
+                if self.game.player.is_on_tile(tile):
+                    self.game.player.on_ground = True  # player is on top of the tile
 
-    def is_colliding_with_block(self, player, block):
-
+    def is_colliding_with_tile(self, player, tile):
         player_left = player.pos.x - player.size[0] / 2
         player_right = player.pos.x + player.size[0] / 2
         player_top = player.pos.y - player.size[1] / 2
         player_bottom = player.pos.y + player.size[1] / 2
 
-        block_left = block.center[0] - block.size[0] / 2
-        block_right = block.center[0] + block.size[0] / 2
-        block_top = block.center[1] - block.size[1] / 2
-        block_bottom = block.center[1] + block.size[1] / 2
+        self.tile_left = tile[1]  - constants.TILE_SIZE / 2 # tile[1] is the x-coordinate of the tile
+        self.tile_right = tile[1] + constants.TILE_SIZE / 2
+        self.tile_top = tile[2] -  constants.TILE_SIZE / 2  # tile[2] is the y-coordinate of the tile
+        self.tile_bottom = tile[2] + constants.TILE_SIZE / 2
 
-        # check for collision
-        return (player_right > block_left and player_left < block_right and player_bottom > (block_top) and player_top < block_bottom)
+        return (player_right > self.tile_left and player_left < self.tile_right and
+                player_bottom > self.tile_top and player_top < self.tile_bottom)
 
-    def resolve_block_collision(self, player, block):
 
-        # calculate overlap in x and y
+    def resolve_tile_collision(self, player, tile):
         overlap_x = min(
-            abs(player.pos.x + player.size[0] / 2 - (block.center[0] - block.size[0] / 2)),
-            abs(player.pos.x - player.size[0] / 2 - (block.center[0] + block.size[0] / 2))
-        )
-        overlap_y = min(
-            abs(player.pos.y + player.size[1] / 2 - (block.center[1] - block.size[1] / 2)),
-            abs(player.pos.y - player.size[1] / 2 - (block.center[1] + block.size[1] / 2))
+            abs(player.pos.x + player.size[0] / 2 - tile[1]),
+            abs(player.pos.x - player.size[0] / 2 - (tile[1] + constants.TILE_SIZE))
         )
 
-        # resolve collision based on overlap
+        overlap_y = min(
+            abs(player.pos.y + player.size[1] / 2 - tile[2]),
+            abs(player.pos.y - player.size[1] / 2 - (tile[2] + constants.TILE_SIZE))
+        )
+
         if overlap_x < overlap_y:
             # horizontal collision
-            if player.pos.x < block.center[0]:
-                player.pos.x = block.center[0] - block.size[0] / 2 - player.size[0] / 2
-            else:
-                player.pos.x = block.center[0] + block.size[0] / 2 + player.size[0] / 2
+            if player.pos.x < self.tile_left:
+                player.pos.x = self.tile_left - player.size[0] / 2
+            elif player.pos.x > self.tile_right:
+                player.pos.x = self.tile_right + player.size[0] / 2
             player.vel.x = 0
         else:
             # vertical collision
-            if player.pos.y < block.center[1]:
-                player.pos.y = block.center[1] - block.size[1] / 2 - player.size[1] / 2
+            if player.pos.y < self.tile_top:
+                player.pos.y = self.tile_top - player.size[1] / 2
+                player.on_ground = True
+
+
             else:
-                player.pos.y = block.center[1] + block.size[1] / 2 + player.size[1] / 2
+                player.pos.y = self.tile_top + constants.TILE_SIZE + player.size[1] / 2
                 player.vel.y = 0
 
+    def is_on_tile(self, player, tile):
+        player_bottom = player.pos.y + player.size[1] / 2
 
+        if abs(player_bottom - self.tile_top) < 5:  # tolerance for inaccuracies
+            player_left = player.pos.x - player.size[0] / 2
+            player_right = player.pos.x + player.size[0] / 2
+            tile_left = tile[1]
+            tile_right = tile[1] + constants.TILE_SIZE
+
+            if player_right > tile_left and player_left < tile_right:
+                return True
+
+        return False
+
+
+# Create frame and start game
 frame = simplegui.create_frame("Mushroom Guy", constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
 game = Game()
 frame.set_draw_handler(game.draw)
 frame.set_keydown_handler(game.keyDown)
 frame.set_keyup_handler(game.keyUp)
-
 frame.start()
